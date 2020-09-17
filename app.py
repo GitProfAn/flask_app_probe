db761f749d30d6b53c98f514c9378e039dba0b5e
GitProfAn <serzhe@mail.ru>
2020-09-17 17:01:25 +0300

    new_commit

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask 
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/<name>')
-def index(name):
-    return f'<h1>Hello,  Big World! and {name}</h1>'
+@app.route('/')
+def index():
+    return '<h1>Hello, World!</h1>'
+
+@app.route('/home')
+def home():
+    return '<h1>You are on the home page!</h1>'
+
+@app.route('/json')
+def json():
+    return jsonify({'key' : 'value', 'listkey' : [1,2,3]})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
